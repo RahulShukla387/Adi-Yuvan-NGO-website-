b71af642297c19f27d3b4faf74aba5d1f5109e22
RahulShukla387 <KnowMeTechnical@gmail.com>
2025-07-21 03:35:14 +0530

    fixed images

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ app.get("/showAll", async(req, res)=>{
 app.get("/home",(req, res)=>{
   res.render("home.ejs");
 })
-app.get("/",(req, res)=>{
-  res.render("showAll.ejs");
+app.get("/", async (req, res)=>{
+    const allImages =await Image.find({});
+  res.render("showAll.ejs", {images: allImages});
 })
